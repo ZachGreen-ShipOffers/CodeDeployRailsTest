8f8cfaaac396b0e2d3c2426bc0d6fbd0a5b98b83
Zachary Green <zachjamesgreen@gmail.com>
2016-05-05 14:46:23 -0600

    edits to scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yum install -y epel-release
-yum install -y --enablerepo=epel nodejs npm
-yum install -y git curl gpg gcc gcc-c++ make
+sudo apt-get install -y nodejs curl build-essential libssl-dev libreadline-dev zlib1g-dev
--- scripts/install_rails_deps.sh
+++ scripts/install_rails_deps.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-
-source ~/.bash_profile
-
-rbenv install 2.3.0
-
-gem install bundler
-
 cd /var/app/CodeDeployRailsTest
 
 bundle install --deployment --without development test
